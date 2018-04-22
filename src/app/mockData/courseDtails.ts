65fe10944e1d604d7d6f5698a1fa5c45a625c566
Trinethra Reddy Alamur <talamur@innominds.com>
2018-04-22 17:34:37 +0530

    Added course details for all courses

--- src/app/mockData/courseDtails.ts
+++ src/app/mockData/courseDtails.ts
@@ -1,12 +1,85 @@
 export const COURSES = [
   {
-    name: 'Sales Force',
+    name: 'Salesforce Lightning Training',
     durationDetails: {
       time: '30Hrs',
       labSession: 20,
       learners: 100
     },
     description: 'Salesforce Lightning Training helps to Learn how to use the new Salesforce user interface efficiently for Salesforce Lightning Experience. We walk you through each section and function of this new user interface to help you make the transit ion from the Salesforce Classic user interface. Salesforce Lightning is a component-based framework that is designed to ease the business processes which in turn will aid you in selling faster, smarter, at your convenient pace and place.',
+    trackData: [{
+      type: 'Regular Track',
+      duration: '30 Hrs',
+      hours: '1hr/day',
+      triningMode: 'Online Classroom'
+    }, {
+      type: '	Weekend Track',
+      duration: '8 Weekends',
+      hours: '2 Hours a day',
+      triningMode: 'Online Classroom'
+    }, {
+      type: 'Fast Track',
+      duration: '5 Days',
+      hours: '6 Hours a day',
+      triningMode: 'Online Classroom'
+    }]
+  },
+  {
+    name: 'Data Science',
+    durationDetails: {
+      time: '30Hrs',
+      labSession: 20,
+      learners: 100
+    },
+    description: 'Data Science With Python Training helps to learn about the Various data formats and how they can be changed, Data reliability, efficiency and affordability, Deriving information from data, Designing modules, Installation f Anaconda Python Distribution, introduction to the field of data science, Python functionality and features.With this course you will learn to work with Python packages such as PROC SQL and various statistical procedures, Understanding and neglecting the unwanted strategies, Understanding the concepts of calculus in this approach. Enroll & get certified now.',
+    trackData: [{
+      type: 'Regular Track',
+      duration: '30 Hrs',
+      hours: '1hr/day',
+      triningMode: 'Online Classroom'
+    }, {
+      type: '	Weekend Track',
+      duration: '8 Weekends',
+      hours: '2 Hours a day',
+      triningMode: 'Online Classroom'
+    }, {
+      type: 'Fast Track',
+      duration: '5 Days',
+      hours: '6 Hours a day',
+      triningMode: 'Online Classroom'
+    }]
+  }, {
+    name: 'Web Technologies',
+    durationDetails: {
+      time: '30Hrs',
+      labSession: 20,
+      learners: 100
+    },
+    description: 'Web Development tutorial for beginners covering HTML, Ajax, CSS, XML, JavaScript, HTML5, jQuery, AngularJS, ASP.NET, ASP.NET MVC, Apache HTTP Server etc. This online tutorial will help you learn all the basic and advanced concepts of Web Programming languages with a very simple and step by step approach. The simple programs and examples taught in each chapter will help you learn all the principles and concepts very quickly and easily.',
+    trackData: [{
+      type: 'Regular Track',
+      duration: '30 Hrs',
+      hours: '1hr/day',
+      triningMode: 'Online Classroom'
+    }, {
+      type: '	Weekend Track',
+      duration: '8 Weekends',
+      hours: '2 Hours a day',
+      triningMode: 'Online Classroom'
+    }, {
+      type: 'Fast Track',
+      duration: '5 Days',
+      hours: '6 Hours a day',
+      triningMode: 'Online Classroom'
+    }]
+  }, {
+    name: 'Node JS',
+    durationDetails: {
+      time: '30Hrs',
+      labSession: 20,
+      learners: 100
+    },
+    description: 'Node.js Training makes you an expert in using Node.js to execute scripts, Representational State Transfer, rest and reading POST data, Connecting to RDBMS and NoSQL databases and Creating a package. etc and become certified at end of the Node.js Course.',
     trackData: [{
       type: 'Regular Track',
       duration: '30 Hrs',
